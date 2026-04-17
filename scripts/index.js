33a4fb8bf26d8a0190638ca785ba0b1b9eac3ec2
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-17 11:54:05 +0200

    creazione della chiamata base api per controllo

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,15 @@
 //@ts-check
 'use strict';
+const apiPosts = 'https://lanciweb.github.io/demo/api/pictures/';
+
+
+fetch(apiPosts)
+    .then((response) => {
+        return response.json();
+    })
+    .then((json) => {
+        console.log(json);
+    })
+    .catch((error) => {
+        console.error(error);
+    })
